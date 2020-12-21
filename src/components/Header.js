b23669fe6c2993b72622be61c070f7f1997206f3
github username <github email address>
2020-12-21 09:01:39 -0500

    second commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import logo from './img/logo.png';
+
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="banner-box">
+        <img src={logo} alt="" />
+      </div>
+    </div>
+
+  );
+}
+
+export default Header;
