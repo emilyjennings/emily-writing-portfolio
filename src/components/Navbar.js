f967f682f9d82706d2441da80c147d7b3d9c1124
github username <github email address>
2020-12-21 19:00:14 -0500

    styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ function Navbar() {
   return(
     <div className="navbar">
       <a href="https://drive.google.com/file/d/1kPJd09V4318fcDFnG_6U9SjzskIOwFNd/view?usp=sharing">My Resume</a>
-      <a href="">About Me</a>
-      <a href="">Contact</a>
+      <a href="/">About Me</a>
+      <a href="/">Contact</a>
     </div>
   );
 }
